e75c73ae96cff9e5fdcc6d8a0c2fd7379eb7d43d
Jorge <georgiearclopez@gmail.com>
2025-02-25 08:32:49 +0100

    poner en produccion 2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const port = 2908
+const port = process.env.PORT || 4000;
 const { Server } = require('socket.io');
 const { createServer } = require('node:http');
 
